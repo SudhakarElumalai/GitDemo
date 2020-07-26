88e7bf3fba540a3495ee6bf994ff12af8c7bc6b9
Sudhakar <sudha.sporty@gmail.com>
2020-07-26 23:56:13 +0530

    Architect Changes

--- src/test/java/stepDefinition/stepDefinition.java
+++ src/test/java/stepDefinition/stepDefinition.java
@@ -20,11 +20,15 @@ public class stepDefinition {
 	  @When("^User Login into application with username and password$")
 	  public void user_login_into_application_with_username_and_password() throws Throwable {
 	    System.out.println("User successfully logined into the application");
+	    
+		  System.out.println("Passed");
 	  	}
 
 	    @Then("^Home page is populated$")
 	    public void home_page_is_populated() throws Throwable {
 	    	System.out.println("User is on Home Page");   
+	    	 System.out.println("Navgated To Home page");
+			  System.out.println("Passed");
 	    }
 
 	    @And("^Cards are displayed$")
